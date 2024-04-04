d171d808cf5ce665b7aee8bbc9d19791dc7691e0
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2024-04-04 13:12:28 -0400

    api

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+function searchCity(city) {
+let apiKey = "006e1d3aa42ft5cc55o041daac8db188"
+let apiUrl = "https://api.shecodes.io/weather/v1/forecast?query=${city}&key={apiKey}"
+
+}
+
+
+
 function handleSearchsubmit(event) {
     event.preventDefault();
 let searcInput = document.querySelector("#search-form-input");
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = searcInput.value;
- apiKey = "006e1d3aa42ft5cc55o041daac8db188"
+searchCity(searcInput.value);
 }
 
 
