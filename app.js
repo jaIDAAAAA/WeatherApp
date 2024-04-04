f6ecdd74f018ccc84ae7a84dff783abe99aaee97
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2024-04-04 13:08:08 -0400

    api

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function handleSearchsubmit(event) {
 let searcInput = document.querySelector("#search-form-input");
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = searcInput.value;
-
+ apiKey = "006e1d3aa42ft5cc55o041daac8db188"
 }
 
 
