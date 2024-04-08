012527b1d4cfcdd14d7af24a1d0b5fb25b748b5b
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2024-04-08 16:55:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 function updateWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-    console.log(response.data)
+let cityElement = document.querySelector("#city");
+
+cityElement.innerHTML = response.data.city
+temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
@@ -15,8 +18,6 @@ axios.get(apiUrl).then(updateWeather);
 function handleSearchsubmit(event) {
     event.preventDefault();
 let searcInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searcInput.value;
 searchCity(searcInput.value);
 }
 
