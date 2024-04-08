1ea6a2004dd372774ac95d49f635b6099f7c06af
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2024-04-08 18:26:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 function updateWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-let cityElement = document.querySelector("#city");
-let descriptionElement = document.querySelector("#description")
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
 
-console.log(data.response.condition.description);
+    console.log(response.data.condition.description);
 
-cityElement.innerHTML = response.data.city
-descriptionElement.innerHTML = response.data.city
-temperatureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.description;
+    temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
-let apiKey = "006e1d3aa42ft5cc55o041daac8db188";
-let apiUrl = "https://api.shecodes.io/weather/v1/forecast?query=${city}&key={apiKey}"
-axios.get(apiUrl).then(updateWeather);
+    let apiKey = "006e1d3aa42ft5cc55o041daac8db188";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey`};
+
+    axios.get(apiUrl).then(updateWeather);
 }
-
-
-
-function handleSearchsubmit(event) {
+    
+function handleSearchSubmit(event) {
     event.preventDefault();
-let searcInput = document.querySelector("#search-form-input");
-searchCity(searcInput.value);
+    let searchInput = document.querySelector("#search-form-input");
+    searchCity(searchInput.value);
 }
 
-
-let searchFormElement = document.querySelector("#Search-form");
-searchFormElement.addEventListener("submit", handleSearchsubmit);
-searchCity("Lisbon");
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
