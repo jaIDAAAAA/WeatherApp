2b9a8a0dce6abf52d3d2beae6609f1660a3ef52a
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2024-04-08 18:28:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ function updateWeather(response) {
 
 function searchCity(city) {
     let apiKey = "006e1d3aa42ft5cc55o041daac8db188";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey`};
+    let apiUrl = "https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey"};
 
     axios.get(apiUrl).then(updateWeather);
-}
+
     
 function handleSearchSubmit(event) {
     event.preventDefault();
