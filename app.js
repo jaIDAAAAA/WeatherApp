59061eefe1cafef9855fb92960e2958f788dff62
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2024-05-20 23:07:23 -0400

    all

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+function updateWeather(response) {
+    console.log(response.data.current.temperature);
+}
+
+
+function searchCity(city) {
+let apiKey = "006e1d3aa42ft5cc55o041daac8db188";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}units=metric`; 
+axios.get(apiUrl).then(updateWeather);
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value; 
+    searchCity(searchInput.value);
 } 
 
 
