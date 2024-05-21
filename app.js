b9dbc3c933c387f22ddd0ed287f29a3226b39d72
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2024-05-20 21:28:57 -0400

    jss

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 function updateWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
