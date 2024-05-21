eb6eb58b796c2af658918284eec69c446726998b
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2024-05-20 21:43:41 -0400

    JS

--- app.js
+++ app.js
@@ -5,42 +5,12 @@ function updateWeather(response) {
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
-    let date = new Date(response.data.time * 1000);
-    let icon = document.querySelector("#icon")
-    
-    icon.innerHTML = `<img src =${response.data.condition.icon_url} class="weather-emoji"/>`
+    let iconElement = document.querySelector("#icon"); 
+    iconElement.innerHTML = <img src="${response.data.condition.icon_url}" class="weather-emoji"/>; 
     cityElement.innerHTML = response.data.city;
-    timeElement.innerHTML = formatDate(date);
+    timeElement.innerHTML = formatDate(new Date(response.data.time * 1000)); 
     descriptionElement.innerHTML = response.data.description;
-    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;  
-    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-    temperatureElement.innerHTML = Math.round(temperature);
-
+    humidityElement.innerHTML = ${response.data.temperature.humidity}%;
+    windSpeedElement.innerHTML = ${response.data.wind.speed}km/h;
+    temperatureElement.innerHTML = Math.round(response.data.temperature.value); 
 }
-function formatDate(date) {
-    
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    let day = days[date.getDay()];
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
-    }
-
-    return `${day} :${hours}:${minutes}`;
-}
-
-    
-function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${apiKey}&units=metric`;
-    console.log(apiUrl);
-    axios.get(apiUrl).then(updateWeather);
-}
-
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
