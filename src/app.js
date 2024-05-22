1cadc09bc55fede2a7a4bba2cd260ba23377c31f
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2024-05-22 18:36:21 -0400

    Api

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ function formatDate(date) {
 }
 function searchCity(city) {
 let apiKey = "006e1d3aa42ft5cc55o041daac8db188";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`; 
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`; 
 axios.get(apiUrl).then(updateWeather);
 }
 function handleSearchSubmit(event) {
