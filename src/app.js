8b489747f7b27bea312dc9d18b6f251586b0c88b
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2024-07-15 17:29:28 -0400

    backticks

--- src/app.js
+++ src/app.js
@@ -67,7 +67,7 @@ function displayForecast(response) {
   days.forEach(function (day) {
     forecastHtml =
       forecastHtml +
-      <div class="weather-forecast"> 
+      `<div class="weather-forecast"> 
         <div class="weather-forecast-day">
           <div class="weather-forecast-date">${day}</div> 
           <div class="weather-forecast-emoji">🌤️</div> 
@@ -75,7 +75,7 @@ function displayForecast(response) {
             <strong>°15</strong><div class="weather-forecast-temperature">°9</div>
           </div> 
         </div>
-      </div>; 
+      </div>`; 
   });
 
   let forecastElement = document.querySelector("#forecast");
