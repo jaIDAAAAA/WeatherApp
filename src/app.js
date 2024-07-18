d9ca0fc7bc46920dafa59d9b9a8d764927ab84a9
jaIDAAAAA <159184980+jaIDAAAAA@users.noreply.github.com>
2024-07-17 20:41:54 -0400

    css and js

--- src/app.js
+++ src/app.js
@@ -77,17 +77,15 @@ function displayForecast(response) {
     forecastHtml =
       forecastHtml +
       `<div class="weather-forecast-day">
-        <div class="weather-forecast-date">${formatDay(day.time)}</div>
-        
-        <img src="${day.condition.icon_url}" class="weather-forecast-icon">
-        </div>
-        <div class="weather-forecast-temperatures">
-          <div class="weather-forecast-temperature">
-            <strong>${Math.round(day.temperature.maximum)}°</strong>
-          </div>
-          <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
-        </div>
-      </div>
+  <div class="weather-forecast-date">${formatDay(day.time)}</div>
+  <img src="${day.condition.icon_url}" class="weather-forecast-icon">
+  <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temperature">
+      <strong>${Math.round(day.temperature.maximum)}°</strong>
+    </div>
+    <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
+  </div>
+</div>
     `; 
   }});
 
